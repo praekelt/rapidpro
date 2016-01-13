9f5f8ae7eb82a27a215b960a0b8dd424978af89e
Bruce McIntyre <bruce.mcintyre@gmail.com>
2016-01-13 08:55:31 +0200

    * removing `--noinput`

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -16,6 +16,6 @@ fi
 
 $pip install -r pip-freeze.txt
 
-$manage migrate --noinput
-$manage collectstatic --noinput
-$manage compress --noinput
+$manage migrate
+$manage collectstatic
+$manage compress
